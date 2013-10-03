66b24f590a71b7da78cc4c41d4005420b0ec323f
takanori <telespectroscope@gmail.com>
2013-10-03 19:47:07 +0900

    Simple BBS function

--- lib/AmPrac/DB/Schema.pm
+++ lib/AmPrac/DB/Schema.pm
@@ -8,9 +8,26 @@ use Teng::Schema::Declare;
 base_row_class 'AmPrac::DB::Row';
 
 table {
-    name 'member';
-    pk 'id';
-    columns qw(id name);
+	name 'sessions';
+	pk 'id';
+	columns qw(
+		id
+		session_data
+	);
 };
+
+table {
+    name 'entry';
+    pk 'entry_id';
+    columns qw(
+		entry_id
+		body
+	);
+};
+# table {
+    # name 'member';
+    # pk 'id';
+    # columns qw(id name);
+# };
 
 1;
--- lib/AmPrac/Web/Dispatcher.pm
+++ lib/AmPrac/Web/Dispatcher.pm
@@ -6,13 +6,43 @@ use Amon2::Web::Dispatcher::Lite;
 
 any '/' => sub {
     my ($c) = @_;
-    return $c->render('index.tt');
+
+	my @entries = $c->db->search('entry', { }, {
+			order_by => {'entry_id' => 'DESC'},
+			limit => 10,
+		},
+	);
+	# my @entries = @{$c->db->selectall_arrayref(
+		# q{SELECT * FROM entry ORDER BY entry_id DESC LIMIT 10},
+		# {Slice => {}}
+	# )};
+	return $c->render(
+		'index.tt' => {
+			entries => \@entries,
+		}
+	);
 };
 
-post '/account/logout' => sub {
-    my ($c) = @_;
-    $c->session->expire();
-    return $c->redirect('/');
+post '/post' => sub {
+	my ($c) = @_;
+
+	if (my $body = $c->req->param('body')) {
+		$c->db->insert(entry => +{
+				body => $body,
+			});
+	}
+	return $c->redirect('/');
 };
+
+# any '/' => sub {
+    # my ($c) = @_;
+    # return $c->render('index.tt');
+# };
+
+# post '/account/logout' => sub {
+    # my ($c) = @_;
+    # $c->session->expire();
+    # return $c->redirect('/');
+# };
 
 1;
